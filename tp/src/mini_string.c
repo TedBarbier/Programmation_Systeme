7e6f9e58e147d499346b39959f566595c71e39d5
TedBarbier <ted.barbier@proton.me>
2024-11-15 13:53:23 +0100

    mini_string qui fonctionne

--- tp/src/mini_string.c
+++ tp/src/mini_string.c
@@ -9,24 +9,36 @@
 //define constant
 #define BUF_SIZE 1024
 
-char buffer[BUF_SIZE];
+char* buffer = NULL;
 int ind = -1;
 
 void mini_printf(char *str)
 {
-    if (ind == -1)
-    {
-        ind = 0;
+    if (str == NULL) {
+        return;
+  }
+    if (ind == -1) {
+        buffer = (char *)mini_calloc(sizeof(char), BUF_SIZE);
+    if (buffer == NULL) {
+      return;
     }
+     ind = 0;
+  }
 
     while (*str)
     {
         buffer[ind++] = *str;
         if (ind == BUF_SIZE || *str == '\n')
         {
-            write(STDOUT_FILENO, buffer, ind);
+            int valid = write(STDOUT_FILENO, buffer, ind);
+            if (valid < 0)
+            {
+                return;
+            }
+            mini_memset(buffer, 0, ind);
             ind = 0;
         }
-        str++;
     }
 }
+
+
